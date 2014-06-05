4ff006a85b5401e74856ffb2ebb102659fe11f9c
Neil Anderson <neil.t.anderson@gmail.com>
2014-06-05 11:11:15 +0100

    update link to point to live

--- src/extensibility/ExtensionManager.js
+++ src/extensibility/ExtensionManager.js
@@ -111,7 +111,7 @@ define(function (require, exports, module) {
         }
 
         CommandManager.register("My Analytics Data", "regard.myanalytics", function(){
-           NativeApp.openURLInDefaultBrowser("http://int-dev.withregard.io:3000/portal#/userevents/" + Regard.getUserId());
+           NativeApp.openURLInDefaultBrowser("http://www.withregard.io/portal#/userevents/" + Regard.getUserId());
         } );
         
         var menu = Menus.getMenu(Menus.AppMenuBar.HELP_MENU);
